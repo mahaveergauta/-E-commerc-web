5cbdafcf6f5fabe6f40092d3b33e40b06b8024a0
mahaveergauta <75659959+mahaveergauta@users.noreply.github.com>
2021-08-30 17:52:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+/*----------------------------------------
+        <!-- JS FOR TOGGLE MENU START  -->------------------------------------------------------
+----------------------------------------*/
+var MenuItems = document.getElementById('MenuItems');
+MenuItems.style.maxHeight = '0px';
+
+function menutoggle(){
+    if(MenuItems.style.maxHeight == '0px'){
+        MenuItems.style.maxHeight = '200px';
+    }
+
+    else{
+        MenuItems.style.maxHeight = '0px';
+    }
+}
+
+
+/*----------------------------------------
+        <!-- JS FOR TOGGLE MENU END  -->------------------------------------------------------
+----------------------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
